114e0fda77862e528a71c7bb78a585fc2acf8fa1
Suleyman Pazarlioglu <s.pazarlioglu1987@hotmail.com>
2023-04-29 17:52:01 +0300

    second commit

---
+++ 3-Operators.js
@@ -0,0 +1,11 @@
+//order of operators in JS is same with JAVA
+console.log(4+4*(-1)-8/2);
+
+let x=5;
+x+5;
+console.log(x);
+
+let one=1;
+let one_again=1;
+let one_string="1";
+let two_string="2";
