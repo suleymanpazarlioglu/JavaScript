bf24a113e1c18f7833fc092bf4ea5f53900fd788
Suleyman Pazarlioglu <s.pazarlioglu1987@hotmail.com>
2023-04-29 19:12:54 +0300

    fifth

--- 3-Operators.js
+++ 3-Operators.js
@@ -1,20 +1,20 @@
 //order of operators in JS is same with JAVA
-console.log(4+4*(-1)-8/2);
+console.log(4 + 4 * (-1) - 8 / 2);
 
-let x=5;
-x+5;
+let x = 5;
+x + 5;
 console.log(x);
 
 //comporison:similar to JAVA with some difference:== and === signs
 
-let one=1;
-let one_again=1;
-let one_string="1";
-let two_string="2";
+let one = 1;
+let one_again = 1;
+let one_string = "1";
+let two_string = "2";
 
 console.log(one == one_again);//true
-console.log(one== one_string);//true-sign is looking for value equaliry
-console.log(one===one_string);//false-looking for both value and data type
+console.log(one == one_string);//true-sign is looking for value equaliry
+console.log(one === one_string);//false-looking for both value and data type
 
 // logical operators sama like JAVA: you have only short circuit && , ||
 console.log(5 < 2 && 2 < 5); // false
@@ -23,9 +23,9 @@ console.log(5 < 2 && 2 < 5); // false
 let num1 = 100;
 let num2 = 10;
 if (num1 > num2) {
-  console.log("number 1 is bigger");
+    console.log("number 1 is bigger");
 } else {
-  console.log("number 2 is bigger");
+    console.log("number 2 is bigger");
 }
 // ternary operator
 let age = 25;
@@ -36,26 +36,26 @@ console.log(isEligible); // true
 let month = 4;
 let dayCount;
 switch (month) {
-  case 1:
-  case 3:
-  case 5:
-  case 7:
-  case 8:
-  case 10:
-  case 12:
-    dayCount = 31;
-    break;
-  case 4:
-  case 6:
-  case 9:
-  case 11:
-    dayCount = 30;
-    break;
-  case 2:
-    dayCount = 28;
-    break;
-  default:
-    dayCount = -1;
-    break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        dayCount = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        dayCount = 30;
+        break;
+    case 2:
+        dayCount = 28;
+        break;
+    default:
+        dayCount = -1;
+        break;
 }
 console.log(dayCount);
---
+++ 5-Arrays.js
@@ -0,0 +1,47 @@
+/**
+ * There is no Collection is JS.In JS we only have Arrays and Map
+ * Not fixed size
+ * There are two important Brachets in JS
+ * [] ----> creates an array
+ * {} ----> creates an object
+ */
+
+let scores = new Array();//creates an empty Array
+let numbers = new Array(10);//creates an array with size 10
+let emptyArray=[]; // creates empty array
+let myNumbers=new Array(1,2,3,5,8,13);
+let colors=["red","green","black","blue"];
+
+console.log(scores);
+console.log(numbers);
+console.log(emptyArray);
+console.log(myNumbers);
+console.log(colors);
+
+//print second element in an array
+console.log(colors[1]);//index based like JAVA
+//add element to an Array
+colors.push("white");//add to element at the end
+console.log(colors);
+
+//add element to beginning of the array
+colors.unshift("orange");//add to element an the beginning
+console.log(colors);
+
+colors.pop();//delete to element from the end
+console.log(colors);
+
+//the size of the array
+console.log(colors.length);
+for(let i=0;i<colors.length;i++){
+    console.log(colors[i]);
+}
+
+//loop with values of array
+for (let value of colors) {
+    console.log(value);
+}
+
+colors.forEach(element =>{
+    console.log(element);
+});
