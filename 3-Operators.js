e49c518830496d0c1118c8b3a4bd5d195238eee1
Suleyman Pazarlioglu <s.pazarlioglu1987@hotmail.com>
2023-04-29 18:03:54 +0300

    third commit

--- 3-Operators.js
+++ 3-Operators.js
@@ -5,7 +5,14 @@ let x=5;
 x+5;
 console.log(x);
 
+//comporison:similar to JAVA with some difference:== and === signs
+
 let one=1;
 let one_again=1;
 let one_string="1";
 let two_string="2";
+
+console.log(one == one_again);//true
+console.log(one== one_string);//true-sign is looking for value equaliry
+console.log(one===one_string);//false-looking for both value and data type
+
