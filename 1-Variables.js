778fd1194a795c7ffe884be2adba111cc712d3c4
Suleyman Pazarlioglu <s.pazarlioglu1987@hotmail.com>
2023-04-29 17:47:07 +0300

    First commit

---
+++ 1-Variables.js
@@ -0,0 +1,28 @@
+console.log("Hello");
+
+//Single comment line
+
+/**
+ * Multiple comment line
+ */
+
+let a;
+console.log(typeof a);
+a=5;
+console.log(typeof a);
+a="Suleyman"; // dynamicly change data type
+console.log(typeof a);
+a=true;
+console.log(typeof a);
+
+let x=55, y="oscar";
+console.log(x+y);//concatination
+console.log(x*y);//NaN error
+
+//Naming rules: same with JAVA naming rules
+
+const number = 35;
+//number=45; you cannot re-assign const variables
+console.log(number);
+
+
